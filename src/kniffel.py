bfae86576d189036817ea8f852ffe03487b93963
Tobias Goetz <tobigoetz98@gmail.com>
2022-04-05 12:20:40 +0200

    Pushed for visibility

--- src/kniffel.py
+++ src/kniffel.py
@@ -1,42 +1,74 @@
 """
 Modelling and executing Kniffel
 """
-import random
+from numpy import random
 
 
 class Dice:
+    """
+    Class for modelling a dice
+    """
     def __init__(self):
-        self.dice = [Die()] * 5
+        self.dice = [Die(), Die(), Die(), Die(), Die()]
 
     def count(self, value: int):
+        occurrences = 0
         for die in self.dice:
             if die.value == value:
-                return 1
+                occurrences += 1
+        return occurrences
 
     def roll(self):
         for die in self.dice:
-            if not die.save:
-                die.roll()
+            die.roll()
+
+    def save(self, index: int):
+        self.dice[index].save()
+
+    def un_save(self, index: int):
+        self.dice[index].un_save()
 
 
 class Die:
+    """
+    Class for modelling a die
+    """
     def __init__(self):
         self.value = 0
-        self.save = False
+        self.saved = False
 
     def roll(self):
-        self.value = random.randint(1, 6)
+        if not self.saved:
+            self.value = random.randint(1, 6)
+
+    def save(self):
+        self.saved = True
+
+    def un_save(self):
+        self.saved = False
 
 
 class Kniffel:
+    """
+    Class for modelling a Kniffel game
+    """
     def __init__(self, number_of_players: int):
         self.players = []
         for i in range(number_of_players):
             self.players.append(Player("Player " + str(i + 1)))
         self.active_player = self.players[0]
 
+    def roll(self):
+        self.active_player.roll()
+
+    def save(self, die_index: int):
+        self.active_player.save(die_index)
+
 
 class Player:
+    """
+    Class for modelling a player
+    """
     def __init__(self, username: str):
         self.username = username
         self.block = Block()
@@ -44,11 +76,21 @@ class Player:
         self.rolls = 0
 
     def roll(self):
-        self.dice.roll()
-        self.rolls += 1
+        if self.rolls < 3:
+            self.dice.roll()
+            self.rolls += 1
+            return
+        else:
+            raise Exception("You have already rolled 3 times")
+
+    def save(self, die_index: int):
+        self.dice.save(die_index)
 
 
 class Block:
+    """
+    Class for modelling a block
+    """
     def __init__(self):
         self.upper = UpperBlock()
         self.lower = LowerBlock()
@@ -58,6 +100,9 @@ class Block:
 
 
 class UpperBlock:
+    """
+    Class for modelling the upper block
+    """
     def __init__(self):
         self.ones = UpperCategory("Ones", 1)
         self.twos = UpperCategory("Twos", 2)
@@ -71,6 +116,9 @@ class UpperBlock:
 
 
 class LowerBlock:
+    """
+    Class for modelling the lower block
+    """
     def __init__(self):
         self.three_of_a_kind = Dice()
         self.four_of_a_kind = Dice()
@@ -85,6 +133,9 @@ class LowerBlock:
 
 
 class Category:
+    """
+    Class for modelling a category
+    """
     def __init__(self, name: str):
         self.name = name
         self.dice = Dice()
@@ -94,6 +145,9 @@ class Category:
 
 
 class UpperCategory(Category):
+    """
+    Class for modelling an upper category
+    """
     def __init__(self, name: str, category_value: int = 0):
         super().__init__(name)
         self.category_value = category_value
@@ -103,8 +157,11 @@ class UpperCategory(Category):
 
 
 def main():
-    dice = Dice()
-    print(dice.dice[0].value)
+    kniffel = Kniffel(2)
+    kniffel.roll()
+    for die in kniffel.active_player.dice.dice:
+        print(die.value)
+
 
 
 if __name__ == "__main__":
