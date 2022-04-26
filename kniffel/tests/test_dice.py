760c322587d5424ace1015a56114fc57d717f5c2
Tobias Goetz <tobigoetz98@gmail.com>
2022-04-26 22:25:51 +0200

    Reworked dice tests to parameterized tests reworked initializer for dice

--- kniffel/tests/test_dice.py
+++ kniffel/tests/test_dice.py
@@ -4,6 +4,8 @@ from io import StringIO
 from unittest import TestCase
 from unittest.mock import patch
 
+from parameterized import parameterized
+
 from kniffel.game import Dice
 
 
@@ -11,15 +13,13 @@ class TestDice(TestCase):
     def setUp(self) -> None:
         self.dice = Dice()
 
-    def test_count_1(self):
-        for i in range(5):
-            self.dice.dice[i].value = 1
-        self.assertEqual(self.dice.count(1), 5)
-
-    def test_count_2(self):
-        for i in range(5):
-            self.dice.dice[i].value = i
-        self.assertEqual(self.dice.count(1), 1)
+    @parameterized.expand([
+        ("five_fives", Dice(values=[5, 5, 5, 5, 5]), 5, 5),
+        ("one_ones", Dice(values=[1, 2, 3, 4, 5]), 1, 1)
+    ])
+    def test_count(self, _name, test_dice, number_to_count, expected_value):
+        self.dice = test_dice
+        self.assertEqual(expected_value, self.dice.count(number_to_count))
 
     def test_count_3(self):
         for i in range(5):
