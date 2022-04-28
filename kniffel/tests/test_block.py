f00e8ca06837af7a725a68b991d3ec23bee3de96
swolf2211 <sebastianwolf1996@gmx.de>
2022-04-28 23:14:36 +0200

    merged with develop and changed paths accordingly

--- kniffel/tests/test_block.py
+++ kniffel/tests/test_block.py
@@ -5,7 +5,9 @@ from unittest import mock
 from unittest import TestCase
 from unittest.mock import patch
 
-from kniffel.game import Block, LowerBlock, UpperBlock, Kniffel, Dice
+from kniffel.models.block import Block, LowerBlock, UpperBlock
+from kniffel.models.dice import Dice
+from kniffel.models.category import Kniffel
 
 
 class TestBlock(TestCase):
@@ -15,13 +17,13 @@ class TestBlock(TestCase):
 
     @patch.object(UpperBlock, 'evaluate', return_value=100)
     @patch.object(LowerBlock, 'evaluate', return_value=80)
-    def test_evaluate2(self, mock_lower, mock_upper):
+    def test_evaluate1(self, mock_lower, mock_upper):
         self.block.kniffel_bonus = 0
         self.assertEqual(180, self.block.evaluate())
 
     @patch.object(UpperBlock, 'evaluate', return_value=100)
     @patch.object(LowerBlock, 'evaluate', return_value=80)
-    def test_evaluate3(self, mock_lower, mock_upper):
+    def test_evaluate2(self, mock_lower, mock_upper):
         self.block.kniffel_bonus = 50
         self.assertEqual(230, self.block.evaluate())
 
@@ -33,13 +35,13 @@ class TestBlock(TestCase):
         self.block.submit(self.Dice, 5)
         self.assertEqual(50, self.block.kniffel_bonus)
 
-    @patch('kniffel.game.UpperBlock.submit')
+    @patch('kniffel.models.block.UpperBlock.submit')
     def test_upper_submit(self, mock_upper_submit):
         self.Dice = mock.Mock()
         self.block.submit(self.Dice, 4)
         mock_upper_submit.assert_called()
 
-    @patch('kniffel.game.LowerBlock.submit')
+    @patch('kniffel.models.block.LowerBlock.submit')
     def test_lower_submit(self, mock_lower_submit):
         self.Dice = mock.Mock()
         self.block.submit(self.Dice, 7)
