1b02133890f2da89cdc7b840d255c02e31617fd6
swolf2211 <sebastianwolf1996@gmx.de>
2022-04-29 16:01:38 +0200

    fixed pylint warnings

--- kniffel/tests/test_app.py
+++ kniffel/tests/test_app.py
@@ -1,3 +1,5 @@
+# pylint: disable=C
+# pylint: disable=protected-access
 from unittest import TestCase
 from unittest.mock import patch
 from io import StringIO
@@ -7,20 +9,25 @@ from kniffel.app import main
 
 class TestApp(TestCase):
 
+    @patch('kniffel.models.game.Game.play')
     @patch('pathlib.Path.exists')
-    def test_path_exists(self, mock_exists):
+    def test_path_exists(self, mock_exists, mock_play):
         # check if prints are being executed if path exists
+        # check if game.play is called if path exists
         mock_exists.return_value = True
         with patch('kniffel.models.game.Game.play'):
             with patch('sys.stdout', new=StringIO()) as fake_out:
                 main()
                 expected_text = "Loading game...\nGame loaded!\n"
                 self.assertEqual(fake_out.getvalue(), expected_text)
+                mock_play.assert_called()
 
+    @patch('kniffel.models.game.Game.play')
     @patch('kniffel.models.dice.Dice.roll')
     @patch('pathlib.Path.exists')
-    def test_path_not_exists(self, mock_exists, mock_dice):
+    def test_path_not_exists(self, mock_exists, mock_dice, mock_play):
         # check if prints are being executed if path doesn't exist
+        # check if game.play is called if path doesn't exist
         mock_dice.return_value = ""
         mock_exists.return_value = False
         with patch('kniffel.models.game.Game.play'):
@@ -28,19 +35,4 @@ class TestApp(TestCase):
                 main()
                 expected_text = "Creating new game...\nGame created!\n"
                 self.assertEqual(fake_out.getvalue(), expected_text)
-
-    @patch('kniffel.models.game.Game.play')
-    @patch('pathlib.Path.exists')
-    def test_play_called1(self, mock_exists, mock_play):
-        # check if game.play is called if path exists
-        mock_exists.return_value = True
-        main()
-        mock_play.assert_called()
-
-    @patch('kniffel.models.game.Game.play')
-    @patch('pathlib.Path.exists')
-    def test_play_called2(self, mock_exists, mock_play):
-        # check if game.play is called if path doesn't exist
-        mock_exists.return_value = False
-        main()
-        mock_play.assert_called()
+                mock_play.assert_called()
